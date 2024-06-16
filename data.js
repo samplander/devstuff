2add105f1df896eb94bd0e00b19f6112b63dc17a
samplander <sampie@slickdzn.com>
2024-06-16 21:16:26 +0200

    v0.2

---
+++ data.js
@@ -0,0 +1,27 @@
+const data =[
+    {
+    "name": "John",
+    "surname": "Doe",
+    "jobTitle": "Software Engineer",
+    "employmentDate": "2022-01-01"
+},
+{
+    "name": "Jane",
+    "surname": "Doe",
+    "jobTitle": "Software Engineer",
+    "employmentDate": "2022-01-01"
+},
+{
+    "name": "John",
+    "surname": "Smith",
+    "jobTitle": "Software Engineer",
+    "employmentDate": "2022-01-01"
+},
+{
+    "name": "Jane",
+    "surname": "Smith",
+    "jobTitle": "Software Engineer",
+    "employmentDate": "2022-01-01"
+
+}
+]
